866ff19b0b4744e538b03e35b59ee40709cb19f6
marchawg <marchawg@gmail.com>
2008-12-15 16:27:30 -0500

    Session update

--- source/inc/Session.php
+++ source/inc/Session.php
@@ -5,9 +5,8 @@
 
 	class Session {
 	
-		var $loggedIn;
-		var $referrer; // referring URL (used for redirecting)
-		//var $lastActivity; //to log out users after a certain period of time
+		private $loggedIn;
+		private $referrer;
 		
 		public function __construct(){
 			$this->startSession();
@@ -15,63 +14,60 @@
 		
 		private function startSession() {
 			session_start();
-			
 			$this->loggedIn = false;
-			
 			$this->referrer = isset($_SESSION['url']) ? $_SESSION['url'] : "/";
 		}
 		
 		//check if password and email are set in cookie and if valid
 		private function checkCookie(){
-			
-			if (empty($_COOKIE['email'])	|| empty($_COOKIE['password'])) {
-			
+			if (empty($_COOKIE['username'])) {
 				return false;
-			}
-			else{
-				//validate email and password with db, return true or false depending
-				//dont forget cookie password is stored as sha1 already
-			}
-		}
-		
-		private function isLoggedIn(){
-			
-			//check if session variable is set
-			if($this->loggedIn == false){
-				return checkCookie();
-			}
-			else{
+			} else {
+				$this->loggedIn = true;
 				return true;
 			}
 		}
 		
-		public function login($email = false, $password = false) {
-			
+		private function isLoggedIn(){
+			//check if session variable is set
+			if($this->loggedIn == false){
+				return checkCookie();
+			} else {
+				return true;
+			}
+		}
+		
+		public function login($username = false, $password = false) {
 			//set session variables
 			$this->loggedIn = true;
 			
-			if($email && $password){
-				//set cookie
-				
-				//cookie expiration period
-				$days = 60;
-				$hours = 0;
-				$minutes = 0;
-				//this is gonna get way funkier but i got an idea for the cookie i will try later
-				setcookie(urlencode($email), urlencode(sha1($password)), time() + ((((($days * 24) + $hours) * 60) + $minutes ) * 60));
+			if ($username && $password) {
+				$userCheck = User::checkLogin($username, $password);
+				if ($userCheck == true) {
+					// Login is good, set cookie
+					//cookie expiration period
+					$days = 90;
+					$hours = 0;
+					$minutes = 0;
+					// just set a cookie with the username encoded
+					setcookie("Pickem", urlencode($username), time() + ((((($days * 24) + $hours) * 60) + $minutes ) * 60));
+				} else {
+					// return errors
+					return $userCheck;
+				}
 			}
-			
 			return true;
 		}
 		
 		
 		public function logout() {
-			
 			// Unset session variables.
 			foreach($_SESSION as &$thisIndex){
 				unset($thisIndex);
 			}
 			
+			// remove cookie
+			setcookie("Pickem", "", time() - 3600);
 			session_destroy();
 		}
 	}
